cdd2d6683b1fdc8b36e7e92ccc41be413e8ef9ad
DJ Bowers <dj@dj-bowers.com>
2020-08-11 09:56:40 -0700

    Add socket-api-server and install socket.io

---
+++ socket-api-server.js
@@ -0,0 +1,68 @@
+'use strict'
+
+const serverIo = require('socket.io')
+
+const api_keys = JSON.parse(process.env.SOCKET_API_KEYS || '[]')
+
+function fromKeyList(packet) {
+  let index = api_keys.indexOf(packet[1])
+  logger.info('fromKeyList found:', index)
+  return index > -1
+}
+
+var initialized = false
+
+function socketAPIServer(apis, initFunction, authenticator = fromKeyList) {
+  const server = serverIo.listen(process.env.PORT || 8000)
+
+  server.on('connection', (socket) => {
+    if (!initialized) {
+      initialized = true
+      process.nextTick(initFunction)
+    }
+
+    // this must be first - to block unauthenticated access to the APIs
+    socket.use((packet, next) => {
+      if (socket.auth) return next()
+      else if (packet[0] === 'authenticate' && authenticator(packet)) {
+        socket.auth = true
+        socket.emit('authenticated')
+        return next()
+      } else return next() // silently ignore unauthorized packets
+    })
+
+    socket.on('disconnect', () => {
+      delete socket.auth
+    })
+
+    socket.on('force-disconnect', (...args) => {
+      console.info('socketAPIServer received force-disconnect', ...args)
+      delete socket.auth
+      if (args[0] === 'exit') {
+        console.error(
+          'not an error but disconnect exit received. exiting process'
+        )
+        process.nextTick(() => process.exit())
+      }
+    })
+
+    apis.forEach((api) => {
+      socket.on(api.name, (...args) => {
+        if (!socket.auth) return // silently ignore unauthorized packets
+        try {
+          api.func(...args)
+        } catch (error) {
+          console.error(
+            'socketAPIServer caught error from api',
+            api.name,
+            error
+          )
+        }
+      })
+    })
+  })
+
+  return server
+}
+
+module.exports = socketAPIServer
